1fcbc0a8dd648b2bfa46155679b39ee7180f1abb
ViKiN6 <5100164@mypillar7uwc.ac.za>
2023-11-28 11:56:49 +0200

    First Commit

---
+++ Interger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExercises
+{
+    internal class Interger
+
+        public static int CountAllIntergers(int) 
+    {
+       int positive = 0;
+       int negative = 0;
+
+        for (int i = 0; i < numbers.Length; i++);
+       
+    }
+
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ClassExercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+           
+            int[] list = { -1, 2, 4, 6, 9 };
+           
+    }
+
+
+}
